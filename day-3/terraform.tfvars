6ae09a832c19c92be2a3908fc0c77859a13f6ba7
devops19 <bhashithagedela@gmail.com>
2025-04-17 22:34:54 +0530

    day-3

---
+++ day-3/terraform.tfvars
@@ -0,0 +1,5 @@
+ami_id = "ami-00a929b66ed6e0de6"
+instance_type = "t2.micro"
+s3_bucket = "tuihiejfklsjfi"
+
+
---
+++ day-3/vars.tf
@@ -0,0 +1,17 @@
+variable "ami_id" {
+    type = string
+    default = ""
+
+
+  
+}
+variable "instance_type" {
+    type = string 
+    default = ""
+  
+}
+variable "s3_bucket" {
+    type = string
+    default = ""
+  
+}
