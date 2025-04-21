2d654ec408b4ec67b72033afdd7300462c73305f
devops19 <bhashithagedela@gmail.com>
2025-04-21 18:01:02 +0530

    dependency block created

---
+++ day-4-dependencyblock/main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "name" {
+  
+ami = "ami-00a929b66ed6e0de6"
+instance_type= "t2.micro"
+  
+}
+resource "aws_vpc" "name" {
+    cidr_block = "10.0.0.0/16"
+    depends_on = [ aws_instance.name ]
+  
+}
