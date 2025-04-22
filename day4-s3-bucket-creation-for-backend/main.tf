eef270ab8a40eab6a4569097f3709b0986eba4df
devops19 <bhashithagedela@gmail.com>
2025-04-22 19:25:15 +0530

    day-4 modified and day-5 created

--- day4-s3-bucket-creation-for-backend/main.tf
+++ day4-s3-bucket-creation-for-backend/main.tf
@@ -2,3 +2,16 @@ resource "aws_s3_bucket" "Name" {
     bucket = "remoteconfigstatefile"
     
 }
+
+
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name = "terraform-state-lock-dynamo"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+ 
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
