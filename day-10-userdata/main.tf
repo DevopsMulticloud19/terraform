dc50c38466d65f2a0bcbbccb5ec07b5bb8834b11
devops19 <bhashithagedela@gmail.com>
2025-04-28 17:02:10 +0530

    day-10

--- day-10-userdata/main.tf
+++ day-10-userdata/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "dev" {
      ami = "ami-0e449927258d45bc4"
-    instance_type = "t2.micro"
-    availability_zone = "us-east-1a"
-    user_data = file("test.sh")
+     instance_type = "t2.micro"
+     availability_zone = "us-east-1a"
+     user_data = file("test.sh")
     
       
 
